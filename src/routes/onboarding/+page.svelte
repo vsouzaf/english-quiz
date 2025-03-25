70f378111e71169bfa9182c9d588ef3d75b18ad5
Valtemir <valtemir.souza168@gmail.com>
2025-03-24 22:46:11 -0300

    chore: commit inicial

---
+++ src/routes/onboarding/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Index</title>
+	<meta name="description" content="Index" />
+</svelte:head>
+
+<div>
+	Index
+</div>
---
+++ src/routes/onboarding/Carousel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="@container">
+	<div class="px-4 py-3">
+		<div
+			class="w-full bg-center bg-no-repeat bg-cover flex flex-col justify-end overflow-hidden bg-white @[480px]:rounded-xl min-h-80"
+			style='background-image: url("https://cdn.usegalileo.ai/sdxl10/6d616d33-f98c-417a-8741-66aa4950b9d2.png");'
+		></div>
+	</div>
+</div>
+<h2 class="text-[#111418] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5">Welcome to Aladin</h2>
+<p class="text-[#111418] text-base font-normal leading-normal pb-3 pt-1 px-4 text-center">Get access to the tools you need to invest, spend, and put your money in motion.</p>
+<div class="flex w-full flex-row items-center justify-center gap-3 py-5">
+	<div class="h-2 w-2 rounded-full bg-[#111418]"></div>
+	<div class="h-2 w-2 rounded-full bg-[#dce0e5]"></div>
+	<div class="h-2 w-2 rounded-full bg-[#dce0e5]"></div>
+	<div class="h-2 w-2 rounded-full bg-[#dce0e5]"></div>
+</div>
